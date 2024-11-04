32947e5fc3544de463e3d5bcc439115f77ec7e32
SirPersifal <feda.ermatov@gmail.com>
2024-11-04 22:22:33 +0300

    Add files via upload

---
+++ дз условная конструкция.py
@@ -0,0 +1,38 @@
+name = input("Прошу пароль :")
+first = 64
+second = 56
+third = 16
+
+if first % 8 == 0:
+        if second % 8 == 0:
+         if third % 8 == 0 :
+           print(3)
+
+name_2 = input("прошу пароль повторно :")
+
+first_2 = 56
+second_2 = 15
+third_2 = 56
+
+if first_2 and second_2 and third_2 % 6 == 0 :
+    print(1)
+
+else:
+    print(2)
+name_3 = input("прошу пароль ещё разок :")
+
+first_3 = 45
+second_3 = 66
+third_3 = 11
+
+if first_3 and second_2 and third_3 % 7 == 0 :
+    print (1)
+
+else:
+    print (0)
+
+
+
+
+
+
