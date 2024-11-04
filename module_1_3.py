9f38ebadeb5086cc40884ad996d18c157f252ca8
SirPersifal <feda.ermatov@gmail.com>
2024-11-04 18:38:23 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,13 @@
+from xmlrpc.client import boolean
+
+name = "fedya"
+print (name)
+
+age = 23
+new_age = (age + 5)
+print (age )
+print(new_age)
+
+it_student = True
+print (it_student)
+print (type(age))
