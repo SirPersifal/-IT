f8a4122f5c31c397def34fa19609471162f7414f
SirPersifal <feda.ermatov@gmail.com>
2024-11-04 21:05:17 +0300

    Add files via upload

---
+++ дз переменные.py
@@ -0,0 +1,10 @@
+name = 'курс Pyton ,'
+tasks = 12
+clock = 1.5
+lasting = (clock/tasks)
+
+print(name ,'всего задач :',tasks ,'затрачено времени :', clock , 'среднее время выполнения :',lasting )
+
+print (type(tasks))
+print (type(clock))
+print (type(lasting))
