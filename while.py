eff30ecead3adc3aa9482e93c52c062db9d38061
SirPersifal <feda.ermatov@gmail.com>
2024-11-22 22:24:22 +0300

    Add files via upload

---
+++ while.py
@@ -0,0 +1,13 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+plus = []
+index = 0
+while index < len(my_list):
+    if my_list[index] < 0:
+        break
+    plus.append(my_list[index])
+    index += 1
+print(*plus , sep='\n')
+
+
+
+
