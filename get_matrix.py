8198dea7f4c40dc755caa88c30fe7588fe3f1aae
SirPersifal <feda.ermatov@gmail.com>
2024-11-25 22:51:57 +0300

    Add files via upload

--- get_matrix.py
+++ get_matrix.py
@@ -1,46 +1,22 @@
-def get_matrix (n = 2,m = 2 ,value = 10):
-   matrix = []
-   for i in range (2):
-         row = []
-         for j in range (2):
-             row.append (10)
-             matrix.append(row)
-
-         return matrix
-
-get_matrix(2,2,10)
-
-print(get_matrix (2,2, 10))
-
-def get_matrix (n = 3 , m = 5 , value = 42 ):
-  matrix = []
-  for i in range (3):
-        row = []
-        for j in range (5):
-         row.append(42)
-         matrix.append(row)
-
-        return matrix
-
-get_matrix(3,5,42)
-
-print(get_matrix(3,5,42))
-
-
-def get_matrix (n = 4 , m = 2 , value = 13):
+def get_matrix (n,m ,value):
     matrix = []
-
-    for i in range (4):
+    for i in range (n):
         row = []
-        for j in range (2):
-            row.append(13)
-            matrix.append(row)
+
+        for j in range (m):
+            row.append (value)
+        matrix.append(row)
+
 
     return matrix
 
-get_matrix(4, 2, 13)
+result1 = get_matrix(2,2,10)
+result2 = get_matrix(3,5,42)
+result3 = get_matrix(4,2,13)
 
-print(get_matrix(4, 2, 13))
+print(result1)
+print(result2)
+print(result3)
 
 
 
